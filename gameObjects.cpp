551dc60ec84cfbefb63f895a498d5db675a73aad
wlgcp2 <31551342+wlgcp2@users.noreply.github.com>
2018-04-16 18:51:17 -0500

    added permissions class

--- gameObjects.cpp
+++ gameObjects.cpp
@@ -249,3 +249,32 @@ class Player {
 			return temp.getServerName();
 		}
 } player;
+
+class Permissions {
+	private:
+		bool worldRead;
+		bool worldWrite;
+		bool worldExecute;
+		bool userRead;
+		bool userWrite;
+		bool userExecute;
+
+	public:
+		void setWorldRead(bool worldRead) { this->worldRead = worldRead; }
+		bool getWorldRead() { return worldRead; }
+
+		void setWorldWrite(bool worldWrite) { this->worldWrite = worldWrite; }
+		bool getWorldWrite() { return worldWrite; }
+
+		void setWorldExecute(bool worldExecute) { this->worldExecute = worldExecute; }
+		bool getWorldExecute() { return worldExecute; }
+
+		void setUserRead(bool userRead) { this->userRead = userRead; }
+		bool getUserRead() { return userRead; }
+
+		void setUserWrite(bool userWrite) { this->userWrite = userWrite; }
+		bool getUserWrite() { return userWrite; }
+
+		void setUserExecute(bool userExecute) { this->userExecute = userExecute; }
+		bool getUserExecute() { return userExecute; }
+} permissions;
