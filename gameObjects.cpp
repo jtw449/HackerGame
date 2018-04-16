9616878abffad4ea1cd1a20d9eb2e96926845c84
wlgcp2 <31551342+wlgcp2@users.noreply.github.com>
2018-04-16 18:15:30 -0500

    Added user and player classes

--- gameObjects.cpp
+++ gameObjects.cpp
@@ -94,6 +94,158 @@ class Server {
 
 // User is the player of the game. Will need to keep track of their current state
 class User {
-  int connected_ip;
-  dir* curr;
+	private:
+		string username;
+		string password;
+		Directory* workingDir;
+		list <string> history;
+		bool superUser = false;
+
+	public:
+		User() {
+
+		}
+
+		User(string username, string password) {
+			this->username = username;
+			this->password = password;
+		}
+
+		User(string username, string password, Directory* workingDir) {
+			this->username = username;
+			this->password = password;
+			this->workingDir = workingDir;
+		}
+
+		string getUsername() {
+			return username;
+		}
+
+		void setDirectory(Directory* workingDir) {
+			this->workingDir = workingDir;
+		}
+
+		Directory* getDirectory() {
+			return workingDir;
+		}
+
+		void addHistory(string command) {
+			history.push_back(command);
+		}
+
+		list<string> getHistory() {
+			return history;
+		}
+
+		void setSuperUser(bool superUser) {
+			this->superUser = superUser;
+		}
+
+		bool getSuperUser() {
+			return superUser;
+		}
+
 } user;
+
+class Player {
+	private:
+		list <User*> userList;
+		list <User*>::iterator userListIter;
+
+		list <Server*> serverList;
+		list <Server*>::iterator serverListIter;
+
+		void addUser(User* newUser) {
+			userList.push_back(newUser);
+		}
+
+		bool removeUser() {
+			if (userList.size() > 1) {
+				userListIter = userList.end();
+				userList.erase(userListIter);
+				
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		void addServer(Server* newServer) {
+			serverList.push_back(newServer);
+		}
+
+		bool removeServer() {
+			if (serverList.size() > 1) {
+				serverListIter = serverList.end();
+				serverList.erase(serverListIter);
+
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+
+	public:
+		Player() {
+
+		}
+
+		Player(string username, string password, Directory* workingDir, Server* newServer) {
+			User* player = new User(username, password, workingDir);
+			addUser(player);
+			addServer(newServer);		
+		}
+
+		void serverLogin(User* newUser, Server* newServer) {
+			addUser(newUser);
+			addServer(newServer);
+		}
+
+		bool serverLogout() {
+			if (removeServer() && removeUser())
+				return true;
+			else
+				return false;
+		}
+
+		bool userLogin(User* newUser) {
+			if (userList.size() == 1) {
+				addUser(newUser);
+				return true;
+			}
+			else {
+				if (removeUser()) {
+					addUser(newUser);
+					return true;
+				}
+				else {
+					return false;
+				}					
+			}		
+		}
+
+		User* getCurrentUser() {
+			if (!userList.empty()) {
+				return userList.back();
+			}
+		}
+
+		Server* getCurrentServer() {
+			if (!serverList.empty()) {
+				return serverList.back();
+			}
+		}
+
+		string getCurrentUsername() {
+			User temp = *userList.back();
+			return temp.getUsername();
+		}
+
+		string getCurrentServerName() {
+			Server temp = *serverList.back();
+			return temp.getServerName();
+		}
+} player;
